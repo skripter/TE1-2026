4c026d3103ba38f8f6163b91f304fe22139040cc
skripter <g.doblesiete@gmail.com>
2026-03-11 22:24:04 -0300

    Add files via upload

--- hola.php
+++ hola.php
@@ -1,5 +1,13 @@
 <?php
-echo("Hola mundo!");
+echo("Hola mundo!<br>");
+$i = 2;
+echo "El valor de i es: ".$i;
+$i++;
+//$i = $i + 1;
+echo "<br>Ahora el valor de i es: ".$i;
+/* comentario 
+multilinea
+*/
 ?>
 <br>
 <b>Esto es HTML</b>
