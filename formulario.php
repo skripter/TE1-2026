a74fdae9acbd63b12b720ee9e17759591a0ccd72
skripter <g.doblesiete@gmail.com>
2026-04-08 21:54:39 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Formulario</title>
+</head>
+<body>
+<form method='POST' action='procesar.php'>
+a)&nbsp;<input type='text' name='var_a'>
+<br><br>
+b)&nbsp;<input type='text' name='var_b'>
+<br><br>
+<input type='submit' value='Calcular'>
+</form>
+</body>
+</html>
