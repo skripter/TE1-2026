14d1145b9e5a430d708e9a82aae2fcd47436fd22
skripter <g.doblesiete@gmail.com>
2026-05-06 23:08:15 -0300

    Add files via upload

---
+++ diagramacion/interfaz.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>Estilos CSS</title>
+<link href='./style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class='encabezado'><a href='#'>Encabezado</a></div>
+
+<div class='menusuperior'>
+<a href='#'>Inicio</a> | 
+<a href='./productos.php'>Productos</a> | 
+<a href='#'>Noticias</a> | 
+<a href='#'>Promos</a> | 
+<a href='#'>Contacto</a> | 
+<a href='#'>Acerca de nosotros</a> | 
+</div>
+
+<div class='menulateral'>
+<h3>Menu lateral</h3>
+<a href='#'>Inicio</a><br>
+<a href='./productos.php'>Productos</a><br>
+<a href='#'>Noticias</a><br>
+<a href='#'>Promos</a><br>
+<a href='#'>Contacto</a><br>
+<a href='#'>Acerca de nosotros</a><br>
+<br>
+</div>
+
+<div class='contenido'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eleifend elementum sem, eu pretium orci molestie sed. Quisque sed vulputate metus, ac auctor sem. Phasellus aliquam, felis et iaculis pulvinar, neque urna pharetra mi, non mattis risus lectus vitae odio. Morbi venenatis est dapibus massa lobortis, et sodales velit consectetur. Etiam facilisis enim quis nisi aliquet, venenatis congue felis lacinia. Nulla porta nunc at nunc volutpat suscipit. Nunc sit amet consectetur arcu, eget vehicula justo. Curabitur in condimentum neque, maximus pretium urna. Proin a finibus elit, sit amet tempor urna. Vestibulum quis metus vitae magna vulputate commodo. Vivamus id sapien et erat cursus tristique a imperdiet diam. Vivamus eget laoreet augue. Nam ullamcorper mauris vel arcu dignissim, non ornare justo tempor. Nulla eros turpis, tempor eu consequat quis, laoreet a nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+</div>
+
+<div class='pie'>Pie de pagina</div>
+</body>
+</html>
---
+++ diagramacion/productos.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>Estilos CSS</title>
+<link href='./style.css' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class='encabezado'><a href='#'>Encabezado</a></div>
+
+<div class='menusuperior'>
+<a href='#'>Inicio</a> | 
+<a href='./productos.php'>Productos</a> | 
+<a href='#'>Noticias</a> | 
+<a href='#'>Promos</a> | 
+<a href='#'>Contacto</a> | 
+<a href='#'>Acerca de nosotros</a> | 
+</div>
+
+<div class='menulateral'>
+<h3>Menu lateral</h3>
+<a href='#'>Inicio</a><br>
+<a href='./productos.php'>Productos</a><br>
+<a href='#'>Noticias</a><br>
+<a href='#'>Promos</a><br>
+<a href='#'>Contacto</a><br>
+<a href='#'>Acerca de nosotros</a><br>
+<br>
+</div>
+
+<div class='contenido'>
+Productos
+</div>
+
+<div class='pie'>Pie de pagina</div>
+</body>
+</html>
