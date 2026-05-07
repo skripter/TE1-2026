6c5a8bb101d2b2a4b54db404ff7519a522457639
skripter <g.doblesiete@gmail.com>
2026-05-06 21:40:26 -0300

    Add files via upload

--- estilo.php
+++ estilo.php
@@ -1,10 +1,30 @@
 <html>
 <head>
 <title>Estilos CSS</title>
-<link href='style.css' rel='stylesheet' type='text/css'>
+<link href='./style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <p>Parrafo de texto de prueba</p>
 <p class='rojo'>Parrafo de texto de prueba rojo</p>
+<table class='tabla'>
+<tr>
+<th>Texto</th>
+<th>i</th>
+</tr>
+<tr><td>El valor de i es:</td><td>1</td></tr>
+<tr><td>El valor de i es:</td><td>2</td></tr>
+<tr><td>El valor de i es:</td><td>3</td></tr>
+<tr><td>El valor de i es:</td><td>4</td></tr>
+<tr><td>El valor de i es:</td><td>5</td></tr>
+<tr><td>El valor de i es:</td><td>6</td></tr>
+<tr><td>El valor de i es:</td><td>7</td></tr>
+<tr><td>El valor de i es:</td><td>8</td></tr>
+<tr><td>El valor de i es:</td><td>9</td></tr>
+<tr><td>El valor de i es:</td><td>10</td></tr>
+</table>
+
+<div class='rojo'>Ejemplo de etiqueta DIV</div>
+<br>
+<div>Ejemplo de etiqueta <span class='rojo'>SPAN</span></div>
 </body>
 </html>
